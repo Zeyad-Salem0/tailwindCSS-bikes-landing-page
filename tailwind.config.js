3e06cb998b576464ec28dd2f0dd7136f345084cc
Zeyad <zeyadm12@outlook.com>
2024-06-13 04:24:20 +0300

    hero section done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**@type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./pages/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sobria: ['"Serreria Sobria"'],
+      },
+    },
+  },
+  plugins: [],
+};
